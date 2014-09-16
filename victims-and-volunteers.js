ad397c40661a91b6c80f166b6e3bdbde3d420a5b
Samantha McCormick <omgitzme@gmail.com>
2014-09-16 11:20:41 -0600

    Worked on some of bonus 3

--- victims-and-volunteers.js
+++ victims-and-volunteers.js
@@ -82,7 +82,56 @@
 
 
 
-// Bonus1: Adding a confirm statement (using object/array) - uses a do...while loop
+// // Bonus1: Adding a confirm statement (using object/array) - uses a do...while loop
+
+// var allVictims = [];   // Initialize the array to store the objects
+
+// do {
+// 	// Create a new currentVictim, which will be pushed onto the array
+// 	var currentVictim = {
+// 		name: prompt("Victim name"),
+// 		phone: prompt("Enter phone"),
+// 		street: prompt("Enter street")
+// 	};
+
+// 	// Push the currentVictim onto the allVictims array
+// 	allVictims.push(currentVictim);
+
+// 	var r = confirm("Would you like to add another person?");
+// } while (r);
+
+// // Print entire array of allVictims
+// console.log(allVictims);
+
+
+
+
+// // Bonus2: Searching
+
+// var victimInNeed = prompt("Search for a victim by name! Enter a person's name.");
+
+// for (var count = 0; count < allVictims.length; count++) {
+// 	if (victimInNeed === allVictims[count].name) {
+// 		var victimStreet = allVictims[count].street;   // Set the matching location to "VictimStreet"
+// 		alert(victimInNeed + " lives at " + victimStreet + " Street");
+// 	};
+// };
+
+// // Find out if VictimInNeed's street matches the other victims' streets
+// alert("These are the volunteers that can help: ");
+
+// // The second part of the below if statement filters out the victimInNeed's name from the list of volunteers
+// for (var count = 0; count < allVictims.length; count++) {
+// 	if ((allVictims[count].street === victimStreet) && (allVictims[count].name != victimInNeed)) {
+// 		alert(allVictims[count].name);
+// 	};
+// };
+
+
+
+// Bonus3: Replace prompts and alerts with forms and on-page rendering (user interface)
+
+// B1: Adding a confirm statement (using object/array) - uses a do...while loop
 
 var allVictims = [];   // Initialize the array to store the objects
 
@@ -106,25 +155,27 @@ console.log(allVictims);
 
 
 
-// Bonus2: Searching
+// B2: Searching
 
 var victimInNeed = prompt("Search for a victim by name! Enter a person's name.");
 
 for (var count = 0; count < allVictims.length; count++) {
 	if (victimInNeed === allVictims[count].name) {
-		var victimStreet = allVictims[count].street;
+		var victimStreet = allVictims[count].street;   // Set the matching location to "VictimStreet"
 		alert(victimInNeed + " lives at " + victimStreet + " Street");
 	};
 };
 
-// Find out if George's street matches the other victims' streets
+// Find out if VictimInNeed's street matches the other victims' streets
 alert("These are the volunteers that can help: ");
 
+// The second part of the below if statement filters out the victimInNeed's name from the list of volunteers
 for (var count = 0; count < allVictims.length; count++) {
-	if (allVictims[count].street === victimStreet) {
+	if ((allVictims[count].street === victimStreet) && (allVictims[count].name != victimInNeed)) {
 		alert(allVictims[count].name);
 	};
 };
+
 
 
 
